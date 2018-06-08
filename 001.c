7372748274b4f4a821b1f8c0041ed74a900e3613
Omid Vaziri <vaziri.dev@gmail.com>
2018-06-08 16:35:04 +0000

    Edit typo.

--- 001.c
+++ 001.c
@@ -1,5 +1,5 @@
 //ProjectEuler: Problem 1
-//Find the sume of all the multiples of 3 or 5 below 1000.
+//Find the sum of all the multiples of 3 or 5 below 1000.
 #include <stdio.h>
 
 #define LIMIT 1000
