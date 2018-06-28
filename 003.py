98f25eead6c33d727cfb513f6b9839485d40a563
Omid Vaziri <vaziri.dev@gmail.com>
2018-06-28 16:41:11 -0400

    Largest prime factor, ported from Python2.x.

---
+++ 003.py
@@ -0,0 +1,26 @@
+#ProjectEuler: Problem 3
+#Find the largest prime factor of the number 600851475143
+import math
+import time
+
+def main():
+    limit = 600851475143
+    start = time.time()
+    for num in range(2,limit):
+        if limit % num == 0:
+            factor = limit / num
+            if isPrime(factor):
+                elapsed = time.time() - start
+                print("Largest prime factor %s found in %s seconds." % (factor, elapsed))
+                return
+
+def isPrime(n):
+    n = abs(n)
+    if n % 2 == 0 and n > 2:
+        return False
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+main()
