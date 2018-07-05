7f18228894bb5bf3db27e741d3b2d8a1d021288f
Omid Vaziri <vaziri.dev@gmail.com>
2018-07-05 16:59:59 -0400

    Start of problem 11 solution.

---
+++ 011.c
@@ -0,0 +1,22 @@
+//ProjectEuler: Problem 11
+//Find the greatest product of four adjacent numbers in the 20x20 grid
+#include <stdio.h>
+#include <time.h>
+
+int main(){
+    FILE *fp;
+    int number, number1;
+    
+    fp = fopen("011-grid.txt", "r");
+
+    /* TODO:
+     * 1) Create 2D array (call it 'grid')
+     * 2) Allocate memory for grid to hold 20x20 ints
+     * 3) Read file line by line into grid
+     * 4) Close fp
+     * 5) Do grid arithmetic to find largest product of 4 adjacent
+     * 6) Return value
+     */
+    
+    return 0;
+}
