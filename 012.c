1eb8831f2b35359ba3927fffaa7067adbd0039b6
Omid Vaziri <vaziri.dev@gmail.com>
2018-07-19 22:29:49 -0400

    Completed solution.

--- 012.c
+++ 012.c
@@ -1,32 +1,47 @@
 //ProjectEuler: Problem 12
 //Find the value of 1st triangle # with over 500 divisors
 #include <stdio.h>
+#include <math.h>
 #include <time.h>
-
 typedef enum {false, true} bool;
 
 int main(void){
     struct timespec start, stop;
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
 
-    bool found = false;		//sentinel value
-    int triangleNum = 1;     	//triangle number
-    int triangleCounter = 2; 	//triangle number counter
+    bool found = false;				//sentinel value
+    unsigned long long int triangleNum = 1;    	//triangle number
+    unsigned long int triangleCounter = 2;	//triangle number counter
 
-    int divCounter = 0;		//counter for # of divisors
+    int divCounter = 0;				//divisor counter
 
-    /**** NOTE: FIRST NEED TO INVESTIGATE ALTERNATIVE TYPE THAT WILL NOT OVERFLOW ****/
-    
+    //Main loop
     do{
+	divCounter = 0;				//reset divisor counter
 	triangleNum += triangleCounter++;	//increase triangle number
-	/*check if triangle number has 500+ divisors
-	   1) If even, check all #'s from 1 to sqrt+1
-	   2) If odd, check all #'s from 1 to sqrt+1, counting up by 2
-	   3) In either of the above checks, each divisor found increases divCounter by 2
-	   4) As soon as divCounter hits 500+, found = true*/
+	//Check # of divisors
+	if(triangleNum % 2){
+	    for(int i = 1; i < (int)ceil(sqrt(triangleNum)); i += 2){
+		if(triangleNum % i == 0){ divCounter += 2; }
+		if(divCounter >= 500){
+		    found = true;
+		    break;
+		}
+	    }
+	}
+	else{
+	    for(int i = 2; i < (int)ceil(sqrt(triangleNum)); i++){
+		if(triangleNum % i == 0){ divCounter += 2; }
+		if(divCounter >= 500){
+		    found = true;
+		    break;
+		}
+	    }
+	}
     }while(!found);
+    
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &stop);
     double elapsed = (stop.tv_sec - start.tv_sec) * 1e3 + (stop.tv_nsec - start.tv_nsec) / 1e6;
-    //output value of triangleNum
+    printf("Found %llu! in %.3f microseconds\n", triangleNum, elapsed);
     return 0;
 }
