1bf62bef350b5dfd60c4e6b7899df2a37f5bff2f
Omid Vaziri <vaziri.dev@gmail.com>
2018-07-21 05:55:06 -0400

    Completed file-reading.

--- 013.c
+++ 013.c
@@ -7,21 +7,35 @@
 int main(void){
     FILE *fp;
     int *array;
-    int digit;
+    int number[50] = {0};
+    int i, j;
     
+    //Read first 50-digit number into array
     fp = fopen("013-number.txt", "r");
     array = (int *)malloc(sizeof(int) * 50);
-    for(int i = 0; i < 50; ++i){
-	fscanf(fp, "%1d", &digit);
-	printf("digit #%d = %d\n", i, digit);
+    for(i = 0; i < 50; ++i){
+	fscanf(fp, "%1d", array+i);
+	//debug: printf("digit #%d = %d\n", i, *(array+i));
     }
-    
-    printf("%d", sizeof(int));
-    
+    getc(fp); //skip past newline (\n\r, 2 bytes in UTF-8)
+    //debug: printf("position: %d, value is %c\n", ftell(fp), getc(fp));
+
+    //loop through every line in file (100 lines, 1 already read)
+    for(i = 0; i < 99; ++i){
+	//Read line into local array
+	for(j = 0; j < 50; ++j){
+	    number[j] = getc(fp) - '0';
+	}
+	getc(fp); //skip past newline (\n\r, 2 bytes in UTF-8)
+
+	//DO ADDITION AND REALLOC HERE
+    }
+    fclose(fp);
+
     struct timespec start, stop;
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
     //Solution
-    //fclose(fp);
+    
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &stop);
     double elapsed = (stop.tv_sec - start.tv_sec) * 1e3 + (stop.tv_nsec - start.tv_nsec) / 1e6;
     printf("Solved in %.3f microseconds\n", elapsed);
@@ -29,15 +43,15 @@ int main(void){
 }
 
 /*TODO:
- * Open file (013-number.txt)
- * Allocate memory to hold an array of 50 ints, all set to 0
- * Read first line into newly allocated array
- * Create a local array for holding each line, 50 ints as well
+ * Open file (013-number.txt) - done
+ * Allocate memory to hold an array of 50 ints - done
+ * Read first line into newly allocated array - done
+ * Create a local array for holding each line, 50 ints as well - done
  * Loop until all lines read:
- *   Read next line into local array
+ *   Read next line into local array - done
  *   Add local array to dynamic array, right to left
  *   If left-most addition goes over 9, reallocate dynamic array
  *   Shift digits over accordingly
- * Close file
- * Check and display result
+ * Close file - done
+ * Check and display result - sorta done
  */
