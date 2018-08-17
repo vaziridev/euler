a2e83e93d0bc9b88b0be5a3d1d3608528dfb2e52
Omid Vaziri <vaziri.dev@gmail.com>
2018-08-17 14:57:46 -0400

    Complete max path sum.

--- 018.c
+++ 018.c
@@ -7,56 +7,50 @@
 #define SIZE 3
 #define FILENAME "018-matrix.txt"
 
-void loadMatrix(int matrix[SIZE][SIZE]);
-void printMatrix(int matrix[SIZE][SIZE]);
-int checkBit(int num, int bit);
+void loadMatrix(int matrix[SIZE][SIZE]);  //load from file into matrix[][]
+void printMatrix(int matrix[SIZE][SIZE]); //print contents of matrix[][]
+int totalPaths();                         //returns total # of paths
+int checkBit(int num, int bit);           //checks value at bit position
 
 int matrix[SIZE][SIZE] = {0};
 
 int main(void){
+  int row, col, num, path, sum, i;
+  int paths = 1, champion = 0, championPath = 0;
+
+  //Start timer
   struct timespec start, stop;
   clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
-  int row, col, num, path, sum, i;
-  int totalPaths = 1, champion = 0, championPath = 0;
-
-  //Load matrix, debug output
+  
+  //Load matrix, calculate total # of paths
   loadMatrix(matrix);
   printMatrix(matrix);
+  paths = totalPaths();
+  //printf("Total paths for triangle with %d rows is: %d\n\n", SIZE, paths);
 
-  //Debug output
-  for(i = 1; i < SIZE; ++i){
-    totalPaths *= 2;
-  }
-  printf("Total paths for triangle with %d rows is: %d\n\n", SIZE, totalPaths);
-
-  //Iterating through each possible path
-  for(path = 0; path < totalPaths; ++path){
+  //Check each possible path
+  for(path = 0; path < paths; ++path){
     //Reset sum + counters
     sum = matrix[0][0];
     row = col = 0;
 
-    //Check each bit of path (0 is left, 1 is right)
+    //Check each bit of path and add node to sum (0 is left node, 1 is right)
     for(i = 0; i < SIZE-1; ++i){
-      //Debug output:
-      printf("Bit of path #%d at position %d: %d\n",
-            path, SIZE-1-i, checkBit(path, SIZE-2-i));
       if(!checkBit(path, SIZE-2-i)){ ++row; }
       else{ ++row; ++col; }
-      //Debug output:
-      printf("Matrix[%d][%d]\n", row, col);
-      printf("Sum = %d + %d\n", sum, matrix[row][col]);
       sum += matrix[row][col];
     }
-    printf("Sum of path #%d is %d\n\n", path, sum);
     
+    //Set champion
     if(sum > champion){
       champion = sum;
-      championPath = path+i;
+      championPath = path;
     }
   }
-  printf("\nChampion is %d!\n", champion);
-  
 
+  printf("\nChampion is %d!\nPath #%d\n", champion, championPath); //championPath to be used for later display
+
+  //Report time elapsed
   clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &stop);
   double elapsed = (stop.tv_sec - start.tv_sec) * 1e3 + (stop.tv_nsec - start.tv_nsec) / 1e6;
   printf("Solved in %.3f microseconds\n", elapsed);
@@ -93,10 +87,18 @@ void printMatrix(int matrix[SIZE][SIZE]){
   int row, col, num;
   for(row=1; row<=SIZE; ++row){
     for(col=row; col>=1; --col){
-      printf("Matrix[%d][%d]: %.2d ", row-1, row-col, matrix[row-1][row-col]);
+      printf("%.2d ", matrix[row-1][row-col]);
     }
     printf("\n");
   }
+}
+
+int totalPaths(){
+  int total = 1;
+  for(int i = 1; i < SIZE; ++i){
+    total *= 2;
+  }
+  return total;
 }
 
 int checkBit(int num, int bit){
